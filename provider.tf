0ae697a74bbb3c79dc6fa6a07f31b8b941049110
gabrielnfonseca-bsb-de <gabriel.fonseca.on@gmail.com>
2026-02-13 22:29:06 +0100

    fixed

--- provider.tf
+++ provider.tf
@@ -8,6 +8,12 @@ terraform {
   }
   required_version = ">= 1.2.0"
 
+  backend "s3" {
+    bucket = "terraform-project-bocket-bsb"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+
 }
 
 provider "aws" {
